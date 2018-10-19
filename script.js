82c1acfb377c840a21585c99d9aaa83e34946c9d
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-10-19 19:22:09 +1300

    console log square.target.id

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ const winCombos = [
     [0,4,8],
     [6,4,2]
 ]
+
+const cells = document.querySelectorAll('.cell')
+startGame();
+
+function startGame() {
+    document.querySelector(".endgame").style.display = "none"
+    origBoard = Array.from(Array(9).keys());
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick, false)
+    }
+}
+
+function turnClick(square){
+    console.log(square.target.id)
+}
